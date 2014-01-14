ffa363bc0dbae8b495b1d27eb2f2735aba4ba359
roy schmidt <cleasto@gmail.com>
2014-01-14 11:56:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Folktell::Application.config.secret_key_base = 'a2fc1d7eb4a82e204ff5bf2f9bd6c82d7f7f480d68749dada37b3ef993aebde0d5f295b0cd34d9f4fd5c79a1ae9c5803c8bddd0cf16d071c4cbfce37a0bca587'
